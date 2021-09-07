08988674a2bc99b4ae0514b7ca1c079635bd66a9
Marwan-Zakia <marwanamir.ma@gmail.com>
2021-09-07 15:12:47 +0300

    newfix

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const weatherData = require('./Weather.json');
 const server = express();
 server.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 
 
-server.listen(PORT, () => {
-    console.log(`Hello, I am listening on ${PORT}`);
-})
+
 server.get('/', (req, res) => {
     res.send('Hello from the home route')
 })
@@ -30,15 +28,14 @@ server.get('/citynames', (req, res) => {
     })
     
 
-
+    let newArr = [];
   result.data.map((element) => {
-        let newArr = [];
-        newArr.push(new Forcast(element.weather.description, element.valid_date,element.low_temp,element.high_temp))
      
-        res.status(200).send(newArr);
+        newArr.push(new Forcast(element.weather.description, element.valid_date,element.low_temp,element.high_temp))
+       
 
     });  
-  
+    res.send(newArr);
 
 
     
@@ -55,3 +52,6 @@ server.get('*',(req,res)=>{
     res.status(404).send('Sorry, page not found');
 })
 
+server.listen(PORT, () => {
+    console.log(`Hello, I am listening on ${PORT}`);
+})
